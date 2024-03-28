05990e64b54ffecf924068bcd6ec53ec59c00bba
Yang <yanalves@souunisuam.com.br>
2024-03-28 11:38:35 -0300

    Avaliação Formadora 1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  meuNome: string = "Yan";
+  codigoTurma: string = "ADS0301N";
+  fotoUrl: string = "C:\Users\yansa\OneDrive\Documentos\avalform1\avaliacaoformadora1\src\assets\imgs";
+}
